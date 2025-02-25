266c360fc6bc7a2f42b46daa118a30de4b40496e
crammiee <crammiee.business@gmail.com>
2025-02-26 03:42:11 +0800

    [Refactor][Cram] Used existing components in itineraryitem component

--- client/src/components/itineraryitem.tsx
+++ client/src/components/itineraryitem.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native-ui-lib";
-import { Feather } from "@expo/vector-icons"; // If using react-native-vector-icons
+import { View, Text, Colors } from "react-native-ui-lib";
+import { Row } from "./row"; 
+import { Icon } from "./icon";
+import { Bounceable } from "rn-bounceable";
 
 type ItineraryItemProps = {
   name: string;
@@ -12,45 +14,51 @@ type ItineraryItemProps = {
 export const ItineraryItem: React.FC<ItineraryItemProps> = ({ name, location, startDate, endDate }) => {
   return (
     <View
+      bg-white
+      padding-s4
+      br40
+      marginV-s2
       style={{
-        backgroundColor: "white",
-        padding: 16,
-        borderRadius: 12,
         shadowColor: "#000",
         shadowOpacity: 0.1,
         shadowRadius: 4,
         shadowOffset: { width: 0, height: 2 },
-        marginVertical: 8,
-        position: "relative",
+        minHeight: 120,
+        justifyContent: "space-between", 
       }}
     >
-      {/* Itinerary Name and Location */}
-      <Text style={{ fontSize: 16, fontWeight: "bold", marginBottom: 4 }}>{name}</Text>
-      <Text style={{ fontSize: 14, color: "gray", marginBottom: 8 }}>{location}</Text>
-
-      {/* Dates */}
-      <Text style={{ fontSize: 12, color: "gray", position: "absolute", bottom: 10, left: 16 }}>
-        {startDate} - {endDate}
-      </Text>
-
-      {/* Icons */}
-      <View style={{ position: "absolute", bottom: 8, right: 16, flexDirection: "row", gap: 8 }}>
-        <TouchableOpacity>
-          <Text>
-            <Feather name="edit" size={14} color="gray" />
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>
-            <Feather name="copy" size={14} color="gray" />
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>
-            <Feather name="trash-2" size={14} color="red" />
-          </Text>
-        </TouchableOpacity>
+      {/* Title and Loc */}
+      <View style={{ marginBottom: 20 }}>
+        <Text text60M>{name}</Text>
+        <Text text80L color={Colors.grey40}>{location}</Text>
       </View>
+
+      {/* Bottom Section (Dates & Icons) */}
+      <Row style={{ justifyContent: "space-between" }}>
+        {/* Dates */}
+        <Text text80L color={Colors.grey40}>
+          {startDate} - {endDate}
+        </Text>
+
+        {/* Icons */}
+        <Row>
+          <Bounceable>
+            <Text>
+              <Icon name="pencil" size={18} color={Colors.primary} />
+            </Text>
+          </Bounceable>
+          <Bounceable>
+            <Text>
+              <Icon name="copy" size={18} color={Colors.primary} />
+            </Text>
+          </Bounceable>
+          <Bounceable>
+            <Text>
+              <Icon name="trash" size={18} color={Colors.red30} />
+            </Text>
+          </Bounceable>
+        </Row>
+      </Row>
     </View>
   );
 };
