dbcf3284c2ad755397225b38e26899febf08c7e7
fpdee9 <63832050+fpdee9@users.noreply.github.com>
2025-02-26 00:23:34 +0800

    [Feature][Philippe] Added Settings screen containing My Account settings

--- client/src/screens/settings.tsx
+++ client/src/screens/settings.tsx
@@ -2,67 +2,95 @@ import React, {useEffect, useState} from 'react';
 import {ScrollView} from 'react-native';
 import {Text, View, SegmentedControl, Colors} from 'react-native-ui-lib';
 import {observer} from 'mobx-react';
-import {useNavigation} from '@react-navigation/native';
 import {NavioScreen} from 'rn-navio';
-
 import {Section} from '@app/components/section';
 import {Row} from '@app/components/row';
-import {
-  appearances,
-  appearancesUI,
-  appearanceUIToInternal,
-  languages,
-  languagesUI,
-  languageUIToInternal,
-} from '@app/utils/types/enums';
+import {useServices} from '@app/services';
 import {useAppearance} from '@app/utils/hooks';
 import {useStores} from '@app/stores';
 import {HeaderButton} from '@app/components/button';
-import {services, useServices} from '@app/services';
+import {
+  appearances,
+  appearancesUI,
+  appearanceUIToInternal
+} from '@app/utils/types/enums';
+import {Bounceable} from 'rn-bounceable';
+import {Icon, IconName} from '@app/components/icon';
+import { colors } from '@app/utils/designSystem';
 
-export const Settings: NavioScreen = observer(({}) => {
-  useAppearance();
-  const {ui} = useStores();
-  const {navio} = useServices();
+export const Settings: NavioScreen = observer(() => {
+  useAppearance(); // for Dark Mode
+  const {t, navio} = useServices();
   const navigation = navio.useN();
+  const {ui} = useStores();
 
   // State
   const [appearance, setAppearance] = useState(ui.appearance);
-  const [language, setLanguage] = useState(ui.language);
 
   // Computed
-  const unsavedChanges = ui.appearance !== appearance || ui.language !== language;
-
+  const unsavedChanges = ui.appearance !== appearance;
   const appearanceInitialIndex = appearances.findIndex(it => it === appearance);
   const appearanceSegments = appearancesUI.map(it => ({label: it}));
 
-  const languageInitialIndex = languages.findIndex(it => it === language);
-  const languageSegments = languagesUI.map(it => ({label: it}));
+  // Methods
+  const handleEditAccount = () => {
+    console.log('Edit Account Pressed');
+  };
+  const handleDeleteAccount = () => {
+    console.log('Delete Account Pressed');
+  };
+  const handleAppearanceIndexChange = (index: number) =>
+    setAppearance(appearanceUIToInternal[appearancesUI[index]]);
+  const handleSave = () => {
+    ui.setMany({appearance});
+  }
 
+  const accountActions: {title: string; icon: IconName; onPress: () => void}[] = [
+    {
+      title: 'Edit Account',
+      icon: 'create-outline', // Use a valid icon name
+      onPress: handleEditAccount,
+    },
+    {
+      title: 'Delete Account',
+      icon: 'trash-outline', // Use a valid icon name
+      onPress: handleDeleteAccount,
+    },
+  ];
+  
   // Start
   useEffect(() => {
     navigation.setOptions({
       headerRight: () =>
         unsavedChanges ? <HeaderButton onPress={handleSave} label="Save" /> : null,
     });
-  }, [unsavedChanges, appearance, language]);
-
-  // Methods
-  const handleAppearanceIndexChange = (index: number) =>
-    setAppearance(appearanceUIToInternal[appearancesUI[index]]);
-  const handleLanguageIndexChange = (index: number) =>
-    setLanguage(languageUIToInternal[languagesUI[index]]);
-
-  const handleSave = () => {
-    ui.setMany({
-      appearance,
-      language,
-    });
-  };
+  }, [unsavedChanges, appearance]);
+  
+  // UI Methods
 
   return (
-    <View flex bg-bgColor>
+    <View flex>
       <ScrollView contentInsetAdjustmentBehavior="always">
+        <Section title={'My Account'}>
+          {accountActions.map(action => (
+            <View key={action.title} marginV-s1>
+              <Bounceable onPress={action.onPress}>
+                {/*Color might change*/}
+                <View padding-s3 br30 style={{backgroundColor:Colors.rgba(240, 240, 240, 1),}}>
+                    <Row>
+                    <Icon name={action.icon} size={30}/>
+                    <View flex marginH-s3>
+                      <Text text60R textColor>
+                        {action.title}
+                      </Text>
+                    </View>
+                    <Icon name="chevron-forward" />
+                  </Row>
+                </View>
+              </Bounceable>
+            </View>
+          ))}
+        </Section>
         <Section title={'UI'}>
           <View paddingV-s1>
             <Row>
@@ -71,7 +99,6 @@ export const Settings: NavioScreen = observer(({}) => {
                   Appearance
                 </Text>
               </View>
-
               <SegmentedControl
                 initialIndex={appearanceInitialIndex}
                 segments={appearanceSegments}
@@ -79,25 +106,7 @@ export const Settings: NavioScreen = observer(({}) => {
                 activeColor={Colors.primary}
                 inactiveColor={Colors.textColor}
                 onChangeIndex={handleAppearanceIndexChange}
-              />
-            </Row>
-          </View>
-
-          <View paddingV-s1>
-            <Row>
-              <View flex>
-                <Text textColor text60R>
-                  Language
-                </Text>
-              </View>
-
-              <SegmentedControl
-                initialIndex={languageInitialIndex}
-                segments={languageSegments}
-                backgroundColor={Colors.bgColor}
-                activeColor={Colors.primary}
-                inactiveColor={Colors.textColor}
-                onChangeIndex={handleLanguageIndexChange}
+                style={{width: 175, flexShrink: 0, alignSelf: 'flex-end'}}
               />
             </Row>
           </View>
@@ -106,6 +115,7 @@ export const Settings: NavioScreen = observer(({}) => {
     </View>
   );
 });
-Settings.options = () => ({
-  title: services.t.do('settings.title'),
+
+Settings.options = props => ({
+  title: 'Settings',
 });
