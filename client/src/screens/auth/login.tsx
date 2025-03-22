01759ef920793bc559da844a1b24ab621edcb8f5
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2025-03-22 22:31:16 +0800

    [Feature][Joshua] Added image header for login

--- client/src/screens/auth/login.tsx
+++ client/src/screens/auth/login.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import {Alert, ScrollView} from 'react-native';
-import {View, Text, Image, Assets, TextField, Button} from 'react-native-ui-lib';
+import {Alert, Image, ImageBackground, ScrollView} from 'react-native';
+import {View, Text, Assets, TextField, Button} from 'react-native-ui-lib';
 import {observer} from 'mobx-react';
 import {NavioScreen} from 'rn-navio';
 
@@ -22,6 +22,9 @@ export const AuthLogin: NavioScreen<Props> = observer(({type = 'push'}) => {
   const [email, setEmail] = useState('')
   const [loading, setLoading] = useState(false);
   const [password, setPassword] = useState('');
+
+  const bg_image = require('../../assets/splash-bg-img.png');
+  const logo_image = require('../../assets/asata_logo.png');
 
   // Start
   useEffect(() => {
@@ -56,80 +59,20 @@ export const AuthLogin: NavioScreen<Props> = observer(({type = 'push'}) => {
     logo: require('../../assets/asata_logo.png')
   });
 
-  const Divider = () => <View style={{ width: '100%', height: 1, backgroundColor: 'grey', marginVertical: 10 }} />;
+  const Divider = () => <View style={{ width: '100%', height: 0.5, backgroundColor: 'grey', marginVertical: 10 }} />;
 
   return (
-    <View flex bg-white center>
-      <ScrollView contentInsetAdjustmentBehavior="always">
-        <View flex centerV marginT-s10>
-
-          <View flex centerH marginT-s10>
-            <Image width={250} height={120} assetName='logo' assetGroup='images'/>  
-          </View>
-
-          <View marginT-s6 centerH>
-            <Text text50 primary>LOGIN</Text>
-
-            <View
-              bg-white
-              br30
-              paddingH-s4
-              paddingV-s2
-              marginT-s6
-              marginB-s10
-              style={{width: 300, borderWidth: 1, borderColor: colors.primary, borderRadius: 12}}
-            >
-              <View paddingH-s3 paddingV-s2>
-                <TextField
-                  accent
-                  fieldStyle={{backgroundColor: 'white', borderWidth: 2, borderColor: 'grey', borderRadius: 6, padding: 4}}
-                  label='Username'
-                  labelColor={colors.accent}
-                  labelStyle={{fontWeight: 'bold'}}
-                  placeholder={'Email'}
-                  placeholderTextColor={'grey'}
-                  value={email}
-                  onChangeText={setEmail}
-                  keyboardType="email-address"
-                  inputMode="email"
-                />
-              </View>
-
-              <View paddingH-s3 paddingV-s2>
-                <TextField
-                  accent
-                  fieldStyle={{backgroundColor: 'white', borderWidth: 2, borderColor: 'grey', borderRadius: 6, padding: 4}}
-                  label='Password'
-                  labelColor={colors.accent}
-                  labelStyle={{fontWeight: 'bold'}}
-                  placeholder={'Password'}
-                  placeholderTextColor={'grey'}
-                  value={password}
-                  onChangeText={setPassword}
-                  keyboardType='default'
-                  secureTextEntry={true}
-                />
-              </View>
-
-              <View centerH>
-                <Button
-                  br30 bg-accent white marginT-s4
-                  style={{width: '80%'}}
-                  label={loading ? 'Logging in ...' : 'SIGN IN'}
-                  onPress={signInWithEmail}
-                />
-              </View>
-
-            </View>
-
-            <Divider />
-            <Text accent style={{fontWeight: 500}}>
-              Don't have an account? <Text primary onPress={() => navio.push('AuthSignup')}>Register now</Text>
-            </Text>
-          </View>
-        </View>
-      </ScrollView>
-    </View>
+    <ScrollView contentContainerStyle={{flex: 1, justifyContent: 'center'}}>
+      <ImageBackground source={bg_image} style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        
+          <Image source={logo_image} style={{width: 177, height: 150}} resizeMode='center'/>
+          
+      </ImageBackground>
+      <View flex bg-white center style={{marginTop: -40, height: '66%', flexGrow: 2, borderTopLeftRadius: 40}}>
+        <Text black>Test</Text>
+      </View>
+      
+    </ScrollView>
   );
 });
 
