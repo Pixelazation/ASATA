4685a1d0b949af1a4746236ea7ae442cc3d77c39
fpdee9 <63832050+fpdee9@users.noreply.github.com>
2025-04-29 17:02:06 +0800

    Added Notification Sounds

--- client/src/services/notifications.ts
+++ client/src/services/notifications.ts
@@ -6,7 +6,7 @@ import { Alert } from "react-native";
 Notifications.setNotificationHandler({
     handleNotification: async () => ({
       shouldShowAlert: true,
-      shouldPlaySound: false,
+      shouldPlaySound: true,
       shouldSetBadge: false,
     }),
   });
