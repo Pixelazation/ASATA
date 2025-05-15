e5dccd874e2c02cf5f3cb331b4855cf3208097db
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2025-05-16 00:57:04 +0800

    [Misc][Shua] Activity modal now shows respective details

--- client/src/utils/dateutils.ts
+++ client/src/utils/dateutils.ts
@@ -8,6 +8,18 @@ export const timestampToDateTimeString = (timestamp: string | Date): string => {
   return new Date(timestamp).toLocaleString('en-US', { month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' });
 }
 
+export const monthFromTimestamp = (timestamp: string | Date): string => {
+  return new Date(timestamp).toLocaleString('en-US', { month: 'short' });
+}
+
+export const dayFromTimestamp = (timestamp: string | Date): string => {
+  return new Date(timestamp).toLocaleString('en-US', { day: '2-digit' });
+}
+
+export const timeAndDayOfWeekFromTimestamp = (timestamp: string | Date): string => {
+  return format(new Date(timestamp), "EE, hh:mm a"); // Example: Monday, 12:00 AM
+}
+
 export function dateToDateString(date: Date): string {
   return format(date, "yyyy/MM/dd");
 }
