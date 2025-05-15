46f6658a444a24554074aca7827b26c119e3c38f
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2025-05-16 01:05:10 +0800

    [Fix][Shua] Fixed itinerary details attempting to load on delete

--- client/src/components/iconbutton.tsx
+++ client/src/components/iconbutton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Bounceable } from "rn-bounceable";
 import { Icon, IconName } from "./icon";
 import { Colors } from "react-native-ui-lib";
+import { Pressable } from 'react-native';
 
 type IconButtonProps = {
   name: IconName;
@@ -12,8 +12,8 @@ type IconButtonProps = {
 
 export const IconButton: React.FC<IconButtonProps> = ({ name, color = Colors.primary, size = 24, onPress }) => {
   return (
-    <Bounceable onPress={onPress}>
+    <Pressable onPress={onPress}>
       <Icon name={name} size={size} color={color} />
-    </Bounceable>
+    </Pressable>
   );
 };
