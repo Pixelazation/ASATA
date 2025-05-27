9b9475f7619426af3b946d8ec7eb48cf4ffb5b57
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2025-05-28 00:21:30 +0800

    [Fix][Shua] Fixed picker issues

--- client/src/components/picker-fixed.tsx
+++ client/src/components/picker-fixed.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {View, Text, Picker} from 'react-native-ui-lib';
+import {View, Text, Picker, PickerValue} from 'react-native-ui-lib';
 import { colors } from '../utils/designSystem';
 
 type Props = {
   label: string;
   value: string;
   placeholder: string;
-  items: string[];
-  onValueChange: (itemValue: string) => void;
+  items: any[];
+  onValueChange: (itemValue: any) => void;
 };
 
 export const PickerFixed: React.FC<Props> = ({label, value, placeholder, items, onValueChange}) => {
@@ -19,10 +19,8 @@ export const PickerFixed: React.FC<Props> = ({label, value, placeholder, items, 
       value={value}
       placeholder={placeholder}
       onChange={onValueChange}
-      fieldType={'dropdown'}
       style={{paddingHorizontal: 16}}
-      fieldStyle={{backgroundColor: '#ECF2F0', borderRadius: 100, paddingVertical: 4, marginTop: 4, marginBottom: -16}}
-      useWheelPicker
+      fieldStyle={{backgroundColor: '#ECF2F0', borderRadius: 100, paddingVertical: 4, marginTop: 4}}
     >
       {items.map((item, index) => (
         <Picker.Item key={index} value={item} label={item} />
